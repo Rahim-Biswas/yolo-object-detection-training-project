4219c2a4917feb8916511bb2a1bdb61f8148cbfd
Rahim Biswas <mr.rahim.biswas@gmail.com>
2026-06-22 22:37:56 +0530

    new

--- src/yolo_training_data.py
+++ src/yolo_training_data.py
@@ -1,10 +1,15 @@
 from ultralytics import YOLO
+import wandb
+
+wandb.init(project="gbc_defect_detection_v2", job_type="model_training")
 
 model = YOLO('yolov9c.pt')
 results = model.train(
-    data='/content/training_data/data.yaml',
-    epochs=100,
+    data='/home/ubuntu/yolo_model_training/yolo-object-detection-training-project/data/data.yaml',
+    epochs=150,
     imgsz=640,
-    batch=7,
+    batch=8,
     save=True,
 )
+
+wandb.finish()
